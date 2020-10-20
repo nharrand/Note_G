8104a33376670396de1e448cae68dd36248f5319
nharrand <nicolas.harrand@gmail.com>
2020-10-20 18:05:07 +0200

    up

--- java/src/main/java/se/kth/castor/App.java
+++ java/src/main/java/se/kth/castor/App.java
@@ -4,12 +4,7 @@ import processing.core.PApplet;
 
 public class App  extends PApplet {
     static int n = 0;
-    static int width = 120;
-    static int heigth = 0;
-    static int[] hs = {56, 168, 368, 656, 1032, 1496, 2048, 2688, 3416, 4232, 4232};
     static int[] hs2 = {0, 56, 176, 376, 664, 1040, 1504, 2056, 2696, 3424, 4240, 4240};
-
-    //"#152A84", "#776EAA", "#201315", "#DB1D6B", "#CDE4CF", "#008460", "#DBE479", "#F1C42F", "#E22733"
 
     public void settings(){
         size(4 * (20 + n), hs2[n]);
@@ -18,15 +13,14 @@ public class App  extends PApplet {
     public void setup() {
         Printer p = new Printer(
                 () -> {
-                    fill(0x15,0x2A,0x84);//"#CDE4CF"
-                    stroke(0x15,0x2A,0x84);
+                    fill(0x00, 0x84, 0x60);//"#008460"
+                    stroke(0x00, 0x84, 0x60);
                 },
                 () -> {
-                    fill(0x77,0x6E,0xAA);//"#776EAA"
-                    stroke(0x77,0x6E,0xAA);
+                    fill(0xCD,0xE4,0xCF);//"#CDE4CF"
+                    stroke(0xCD,0xE4,0xCF);
                 },
                 pixel -> point(pixel.x, pixel.y)
-                //pixel -> rect(pixel.x, pixel.y, pixel.w, pixel.h)
         );
         NoteG ng = new NoteG(21 + n, n, p);
         ng.run();
@@ -44,12 +38,7 @@ public class App  extends PApplet {
         }
     }
 
-    public void draw(){
-    }
-
-    public void writeCell(int i) {
-
-    }
+    public void draw(){ }
 
     public static void main(String[] args) {
         int a = 5;
